facd19cf9a71a533dd2371ad07039b1e831b5dd5
Arthur Clarkson <aclarkson@labman.co.uk>
2024-04-03 09:16:53 +0100

    added js lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,7 +12,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "csharp_ls", "omnisharp" }
+                ensure_installed = { "lua_ls", "tsserver", "csharp_ls", "omnisharp" }
             })
         end
     },
@@ -23,6 +23,7 @@ return {
             lspconfig.lua_ls.setup({})
             lspconfig.csharp_ls.setup({})
             lspconfig.omnisharp.setup({})
+            lspconfig.tsserver.setup({})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'D', vim.lsp.buf.definition, {})
             vim.keymap.set('n', '<A-.>', vim.lsp.buf.code_action, {})
